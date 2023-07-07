a562d8da976206f7579707f85010572dd9e17232
danicalderonm <mesiasd.calderon@gmail.com>
2023-07-06 23:20:48 -0400

    configurando todo para subir a render

---
+++ bin/renderlbuild.sh
@@ -0,0 +1,12 @@
+render-build.sh
+#!/usr/bin/env bash
+#exit on error
+set -o errexit 
+
+bundle install 
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec db:drop
+bundle exec db:create
+bundle exec rake db:migrate
+bundle exec rake db:seed
