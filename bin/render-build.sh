37f653ea54d50fba34bfab92f9add89ced212493
danicalderonm <mesiasd.calderon@gmail.com>
2023-07-06 23:33:50 -0400

    volviendo a crear archivo build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,5 @@ set -o errexit
 bundle install 
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec db:drop
-bundle exec db:create
 bundle exec rake db:migrate
 bundle exec rake db:seed
